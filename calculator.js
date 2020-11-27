3abf40e42eefd06c6108490787943f80222d7bf7
mhgitrepo <mehedi1921@gmail.com>
2020-11-27 06:29:51 +0600

    Simple Calculator

---
+++ calculator.js
@@ -0,0 +1,5 @@
+function count (btnValue){
+	document.getElementById('display').value = document.getElementById('display').value + btnValue;
+	// const valueParse = parseInt(document.getElementById('display').value);
+	// return valueParse;
+}
